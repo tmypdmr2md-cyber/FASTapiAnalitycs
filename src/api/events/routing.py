278e098959e468fce3414b650b8dac7a36d98e88
Иванов Александр Ильич <alexanderivanov@MacBook-Pro-Ivanov.local>
2026-05-06 22:41:11 +0300

    update schema, nbs files, routing

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,16 +1,62 @@
+from uuid import UUID, uuid4
+
 from fastapi import APIRouter
+
 # говорим в каком формате мы хотим возвращать данные
-from .schema import EventSchema
+from .schema import EventListSchema, EventSchema, EventCreateSchema, EventUpdateSchema
 
-router = APIRouter() 
+router = APIRouter()
+
+"""
+api methods:
+GET    /api/events/             - возвращает список событий
+POST   /api/events/             - создает новое событие
+GET    /api/events/{event_id}   - возвращает событие по id
+PUT    /api/events/{event_id}   - обновляет описание события по id
+DELETE /api/events/{event_id}   - удаляет событие по id
+"""
 
 
+# GET /api/events/ - возвращает список событий
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     return {
-        "items" : "[1, 2, 3]"
+        "results": [
+            {"id": uuid4()},
+            {"id": uuid4()},
+            {"id": uuid4()},
+        ],
+        "count": 3,
     }
 
+
+# POST /api/events/ - создает новое событие
+@router.post("/")
+def create_event(payload: EventCreateSchema) -> EventSchema:
+    data = payload.model_dump()
+    return {
+        "id": uuid4(),
+        **data,
+    }
+
+
+# GET /api/events/{event_id} - возвращает событие по id
 @router.get("/{event_id}")
-def get_events(event_id: int) -> EventSchema:
+def get_events(event_id: UUID) -> EventSchema:
     return {"id": event_id}
+
+
+# PUT /api/events/{event_id} - обновляет описание события по id
+@router.put("/{event_id}")
+def update_event(event_id: UUID, payload: EventUpdateSchema) -> EventSchema:
+    return {
+        "id": event_id,
+        "path": payload.path,
+        "description": payload.description,
+    }
+
+
+# DELETE /api/events/{event_id} - удаляет событие по id
+@router.delete("/{event_id}")
+def delete_event(event_id: UUID):
+    pass
--- src/api/events/schema.py
+++ src/api/events/schema.py
@@ -1,7 +1,30 @@
-
+from typing import List, Optional
 from pydantic import BaseModel
+from datetime import datetime
+from uuid import UUID
 
 
+"""
+id 
+path 
+description
+"""
 class EventSchema(BaseModel):
-    id: int
+    id: UUID
+    path: Optional[str] = "" 
+    description: Optional[str] = ""
+
     
+
+class EventListSchema(BaseModel):
+    results: List[EventSchema]
+    count: int = 0
+
+class EventCreateSchema(BaseModel):
+    path: str
+    description: str = ""
+
+class EventUpdateSchema(BaseModel):
+    path: str = ""
+    description: str = ""
+
